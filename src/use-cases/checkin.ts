769247c45a9c556b1f435b7e478b169ffb50d49e
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-21 03:21:49 -0300

    feat: add ensure CheckInUseCase throws if distance is higher than 0.1km

--- src/use-cases/checkin.ts
+++ src/use-cases/checkin.ts
@@ -1,9 +1,14 @@
 import { CheckIn } from '@prisma/client'
 import { CheckInRepository } from '@/repositories/interfaces/checkins-repositories'
+import { GymsRepository } from '@/repositories/interfaces/gyms-repositories'
+import { ResourceNotFoundError } from '@/errors/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 interface CheckInUseCaseParams {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -11,11 +16,16 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private checkInRepository: CheckInRepository) {}
+  constructor(
+    private checkInRepository: CheckInRepository,
+    private gymsRepository: GymsRepository,
+  ) {}
 
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseParams): Promise<CheckInUseCaseResponse> {
     const checkInOnSameDay = await this.checkInRepository.findByUserIdOnDate(
       userId,
@@ -24,6 +34,26 @@ export class CheckInUseCase {
 
     if (checkInOnSameDay) {
       throw new Error('Error')
+    }
+
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KM = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KM) {
+      throw new Error('')
     }
 
     const checkIn = await this.checkInRepository.create({
