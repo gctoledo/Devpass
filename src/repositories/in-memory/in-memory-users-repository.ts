ab88c2d48badc14026e17b9b5dbaf1243595f3cf
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-20 23:31:46 -0300

    feat: add in memory db to tests

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,30 @@
+import { UsersRepository } from '@/interfaces/repositores'
+import { User, Prisma } from '@prisma/client'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      created_at: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+}
