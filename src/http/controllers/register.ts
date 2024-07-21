c3d20c4b42907ecfc8222e55f4088ad88234db70
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-20 21:58:27 -0300

    feat: add POST route to create user

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,25 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+export const register = async (req: FastifyRequest, reply: FastifyReply) => {
+  const createUserBodySchema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(7),
+  })
+
+  const { email, name, password } = createUserBodySchema.parse(req.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({ email, name, password })
+  } catch (err) {
+    return reply.status(409).send({ message: 'Email already exists' })
+  }
+
+  return reply.status(201).send()
+}
