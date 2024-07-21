e2b1b4e05c7d19df5d4cb41d4547b3084f1076d6
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-21 01:05:59 -0300

    feat: add GetUserProfileUseCase

---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,27 @@
+import { User } from '@prisma/client'
+import { UsersRepository } from '@/repositories/interfaces/users-repositories'
+import { ResourceNotFoundError } from '@/errors/resource-not-found-error'
+
+interface GetUserProfileUseCaseParams {
+  userId: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseParams): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    return { user }
+  }
+}
