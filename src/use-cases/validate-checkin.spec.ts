2d6d9ae3db132d530424e9f24347e520b35db68b
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-21 19:52:56 -0300

    test: ensure ValidateCheckInUseCase should be able to validate check-in

---
+++ src/use-cases/validate-checkin.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-checkins-repository'
+import { describe, expect, it } from 'vitest'
+import { ValidateCheckInUseCase } from './validate-checkin'
+
+describe('Validate CheckIn Use Case', () => {
+  // beforeEach(() => {
+  //   vi.useFakeTimers()
+  // })
+
+  // afterEach(() => {
+  //   vi.useRealTimers()
+  // })
+
+  const makeSut = () => {
+    const checkInsRepository = new InMemoryCheckInRepository()
+    const sut = new ValidateCheckInUseCase(checkInsRepository)
+
+    return { sut, checkInsRepository }
+  }
+
+  it('should be able to validate check-in', async () => {
+    const { sut, checkInsRepository } = makeSut()
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-1',
+      user_id: 'user-1',
+    })
+
+    const { checkIn } = await sut.execute({ checkInId: createdCheckIn.id })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.checkIns[0].validated_at).toEqual(
+      expect.any(Date),
+    )
+  })
+})
