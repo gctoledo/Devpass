5a23856c351a901384035802a4df27748f56c1b1
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-21 04:26:35 -0300

    feat: add findManyByUserId method to InMemoryCheckInRepository

--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -6,6 +6,14 @@ import { CheckInRepository } from '../interfaces/checkins-repositories'
 export class InMemoryCheckInRepository implements CheckInRepository {
   public checkIns: CheckIn[] = []
 
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = this.checkIns
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+
+    return checkIns
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date')
     const endOfTheDay = dayjs(date).endOf('date')
