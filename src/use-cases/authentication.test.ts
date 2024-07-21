344dac3021364cb08acffb2547c685485b2cda80
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-21 00:28:05 -0300

    test: ensure AuthenticationUseCase should not be able to authenticate if email does not exist

--- src/use-cases/authentication.test.ts
+++ src/use-cases/authentication.test.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { AuthenticateUseCase } from './authentication'
 import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from '@/errors/invalid-credentials'
 
 describe('Authentication Use Case', () => {
   const makeSut = () => {
@@ -26,5 +27,13 @@ describe('Authentication Use Case', () => {
     })
 
     expect(result.user.email).toEqual('john@doe.com')
+  })
+
+  it('should not be able to authenticate if email does not exist', async () => {
+    const { sut } = makeSut()
+
+    const promise = sut.execute({ email: 'john@doe.com', password: 'password' })
+
+    expect(promise).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
