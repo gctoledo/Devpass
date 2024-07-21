993df581b30307f3e194da1e1e8052068e0e3690
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-21 02:36:29 -0300

    feat: add findByUserIdOnDate method to InMemoryCheckInRepository

--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -1,9 +1,29 @@
 import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 import { Prisma, CheckIn } from '@prisma/client'
 import { CheckInRepository } from '../interfaces/checkins-repositories'
 
 export class InMemoryCheckInRepository implements CheckInRepository {
   public checkIns: CheckIn[] = []
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkIn = this.checkIns.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
