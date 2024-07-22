9c1a21a620d158310a6346d157bf62b3ceceb3cd
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-22 15:14:30 -0300

    test: ensure metrics check-in controller should be able to get count of check-ins

---
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -0,0 +1,51 @@
+import { it, describe, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import app from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import prisma from '@/lib/prisma'
+
+describe('Check-In Metrics Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get count of check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Example Gym',
+        description: 'Example description',
+        phone: '11999999999',
+        latitude: -20.5039819,
+        longitude: -48.5582783,
+      },
+    })
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/check-ins/metrics')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkInsCount).toEqual(2)
+  })
+})
--- src/http/controllers/check-ins/metrics.ts
+++ src/http/controllers/check-ins/metrics.ts
@@ -8,5 +8,5 @@ export const metrics = async (req: FastifyRequest, reply: FastifyReply) => {
     userId: req.user.sub,
   })
 
-  return reply.status(201).send({ checkInsCount })
+  return reply.status(200).send({ checkInsCount })
 }
