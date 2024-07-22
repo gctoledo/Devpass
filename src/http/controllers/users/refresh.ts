889390945e3e9fbfb60acf6a1dadf43613c594fb
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-22 16:19:18 -0300

    feat: add refresh token route

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export const refresh = async (req: FastifyRequest, reply: FastifyReply) => {
+  await req.jwtVerify({ onlyCookie: true })
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: req.user.sub,
+      },
+    },
+  )
+
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: req.user.sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true, // https
+      sameSite: true, // only same site can access
+      httpOnly: true,
+    })
+    .status(200)
+    .send({ token })
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from './register'
 import { authentication } from './authentication'
 import { profile } from './profile'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export const usersRoutes = async (app: FastifyInstance) => {
   app.post('/users', register)
   app.post('/session', authentication)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
