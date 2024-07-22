4f80d8f1c1dd51cc6c0459ec582f8f5fffcc1275
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-22 01:21:01 -0300

    test: ensure register controller should be able to register

---
+++ src/http/controllers/register.spec.ts
@@ -0,0 +1,21 @@
+import { it, describe, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import app from '@/app'
+
+describe('Register Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({ name: 'John Doe', email: 'john@doe.com', password: 'password' })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
