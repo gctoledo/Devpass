8843e7ddaaa067cc5c5dd938df159a6e2291cae4
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-22 14:56:25 -0300

    test: ensure create gym controller should be able to create gym

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { it, describe, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import app from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+
+describe('Create Gym Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Example Gym',
+        description: 'Example description',
+        phone: '11999999999',
+        latitude: -20.5039819,
+        longitude: -48.5582783,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
