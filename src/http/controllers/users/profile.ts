8761134ddd3129d63f5869553b6f92437ae2e5df
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-23 00:10:33 -0300

    fix: ensure returns 400 if use case throws ResourceNotFoundError

--- src/http/controllers/users/profile.ts
+++ src/http/controllers/users/profile.ts
@@ -1,10 +1,17 @@
+import { ResourceNotFoundError } from '@/errors/resource-not-found'
 import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export const profile = async (req: FastifyRequest, reply: FastifyReply) => {
   const getUserProfile = makeGetUserProfileUseCase()
 
-  const { user } = await getUserProfile.execute({ userId: req.user.sub })
+  try {
+    const { user } = await getUserProfile.execute({ userId: req.user.sub })
 
-  return reply.status(200).send({ ...user, password: undefined })
+    return reply.status(200).send({ ...user, password: undefined })
+  } catch (err) {
+    if (err instanceof ResourceNotFoundError) {
+      return reply.status(400).send({ message: err.message })
+    }
+  }
 }
