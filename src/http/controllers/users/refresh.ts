cb2a3b1603704b101a6f1cb1be34558809858677
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-23 00:44:36 -0300

    fix: ensure to reply 401 if refresh token is not provided

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -1,36 +1,40 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export const refresh = async (req: FastifyRequest, reply: FastifyReply) => {
-  await req.jwtVerify({ onlyCookie: true })
+  try {
+    await req.jwtVerify({ onlyCookie: true })
 
-  const { role } = req.user
+    const { role } = req.user
 
-  const token = await reply.jwtSign(
-    { role },
-    {
-      sign: {
-        sub: req.user.sub,
+    const token = await reply.jwtSign(
+      { role },
+      {
+        sign: {
+          sub: req.user.sub,
+        },
       },
-    },
-  )
+    )
 
-  const refreshToken = await reply.jwtSign(
-    { role },
-    {
-      sign: {
-        sub: req.user.sub,
-        expiresIn: '7d',
+    const refreshToken = await reply.jwtSign(
+      { role },
+      {
+        sign: {
+          sub: req.user.sub,
+          expiresIn: '7d',
+        },
       },
-    },
-  )
+    )
 
-  return reply
-    .setCookie('refreshToken', refreshToken, {
-      path: '/',
-      secure: true, // https
-      sameSite: true, // only same site can access
-      httpOnly: true,
-    })
-    .status(200)
-    .send({ token })
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true, // https
+        sameSite: true, // only same site can access
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token })
+  } catch (err) {
+    return reply.status(401).send({ message: 'Unauthorized' })
+  }
 }
